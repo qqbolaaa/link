176ac183e2488237c814f4826d8a06cb5414fc7a
qqbolaaa <qqbolaaa@gmail.com>
2026-05-12 20:18:17 +0800

    Update qqbola69.js

--- qqbola69.js
+++ qqbola69.js
@@ -1,3 +1,3 @@
 document.getElementById('yourform').submit();
-function linkqqbola69(){location.replace("https://www.youtube.com/");}
-function linklivechat(){location.replace("https://www.youtube.com/");}
+function linkqqbola69(){location.replace("https://qqbola69.com/");}
+function linklivechat(){location.replace("https://qqbola69.com/");}
