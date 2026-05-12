4731383012cb4f6baa8ff79172201f77e8a788eb
qqbolaaa <qqbolaaa@gmail.com>
2026-05-12 20:19:23 +0800

    Update qqbola69.js

--- qqbola69.js
+++ qqbola69.js
@@ -1,3 +1,3 @@
 document.getElementById('yourform').submit();
 function linkqqbola69(){location.replace("https://qqbola69.com/");}
-function linklivechat(){location.replace("https://qqbola69.com/");}
+function linklivechat(){location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=13374150&group=132");}
