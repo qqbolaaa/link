a941609757aad26026c1dbf6451f125ded3ffd70
judisuhu1-ux <judisuhu1@gmail.com>
2026-06-10 02:43:16 +0800

    Update qqbola69.js

--- qqbola69.js
+++ qqbola69.js
@@ -1,3 +1,3 @@
 document.getElementById('yourform').submit();
-function linkqqbola69(){location.replace("https://linkqqbola69.pages.dev/");}
+function linkqqbola69(){location.replace("https://linkqqbola69.com/");}
 function linklivechat(){location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=13374150&group=132");}
